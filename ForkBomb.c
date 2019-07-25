457181ec26452bea05aa8095c2b25e9c40593c8b
Chris Katevas <44655162+katevaschris@users.noreply.github.com>
2019-07-25 20:25:57 +0300

    Update ForkBomb.c

--- ForkBomb.c
+++ ForkBomb.c
@@ -1,3 +1,13 @@
+
+/*
+	Main Authors: Katevas Chris
+	p18068
+	github: https://github.com/katevaschris
+	
+	Athens 15, July 2019
+	
+	Protected by GNU GPLv3
+*/
 #include <unistd.h>
 #include <stdlib.h>
 #include <stbool.h>
