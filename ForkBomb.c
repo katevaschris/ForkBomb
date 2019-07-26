1cd3866311fc120a346fbd3c9f82399a1f4c36c4
Chris Katevas <44655162+katevaschris@users.noreply.github.com>
2019-07-26 18:59:46 +0300

    Update ForkBomb.c

--- ForkBomb.c
+++ ForkBomb.c
@@ -1,33 +1,31 @@
 /*
-	Authors: Katevas Chris
+Authors: Katevas Chris
 
-	github: https://github.com/katevaschris
+github: https://github.com/katevaschris
 	
-	Planet: Earth 
-	Date: 	28, May 2017
+Planet: Earth 
+Date: 	
 	
-	Version 1.0
-	Function:
-		A simple peedometer that counts kilometers per hour/ or miles per
-		hour by accesing phone's GPS
+Version 1.0
+Function:
 
-	Copyright (C)
-	Protected by GNU GPLv3
-
-	~ 
+Copyright (C)
+Protected by GNU GPLv3
+~ 
 	
 	Greetings Mate!
 	¯\_(ツ)_/¯ 
 	
 	I don't mind what you use this code for! it wasn't made for professional use.
-	Instead, it was made for edducational purposes and for fun.
+	Instead, it was made for educational purposes and for fun.
 	Please hack this, change it and use it in any way you see fit.
 	Please, be acknowledged that I take no responsibility for anything bad that 
-	happens,to you as a result of your actions. However this code is protected by GNU GPLv3,
+	happens, to you as a result of your actions. However this code is protected by GNU GPLv3,
 	this means you must attribute me if you use it. Have fun and...
 	Cheers :D
 	
-	~
+~
+
 */
 #include <unistd.h>
 #include <stdlib.h>
